dfbdb3ac279310bd3f5bf057a4c743ed0dfc0680
amccausl <amccausl@9b09796e-d729-0410-bf65-cd7fb1a0d812>
2008-11-22 05:15:51 +0000

    Moved services controller.
    git-svn-id: svn+ssh://mathsoc.uwaterloo.ca/mathsoc/svn/www/branches/zend@167 9b09796e-d729-0410-bf65-cd7fb1a0d812

--- application/default/controllers/ServicesController.php
+++ application/default/controllers/ServicesController.php
@@ -4,9 +4,9 @@ require_once 'Zend/Controller/Action.php';
 
 class Services_IndexController extends Zend_Controller_Action
 {
-    public function indexAction()
-    {
-    }
+	public function indexAction()
+	{
+	}
 
 	// Present prewritten pages
 	public function cifAction() {}
