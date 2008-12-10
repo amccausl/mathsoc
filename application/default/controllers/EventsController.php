38af2a6c17866d94daeb40af986608aa7adee664
amccausl <amccausl@9b09796e-d729-0410-bf65-cd7fb1a0d812>
2008-12-10 04:39:27 +0000

    Added temporary fix for event calendar system.  Hookup php icalendar to google feed.
    git-svn-id: svn+ssh://mathsoc.uwaterloo.ca/mathsoc/svn/www/branches/zend@182 9b09796e-d729-0410-bf65-cd7fb1a0d812

--- application/default/controllers/EventsController.php
+++ application/default/controllers/EventsController.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'MathSocAction.inc';
-require_once '../application/models/announceDB.inc';
 
 class EventsController extends MathSoc_Controller_Action
 {
@@ -9,11 +8,11 @@ class EventsController extends MathSoc_Controller_Action
 
 	public function init()
 	{	parent::init();
-		$this->db = $this->db = new AnnounceDB();
+		//$this->db = $this->db = new AnnounceDB();
 	}
 
 	public function indexAction()
-	{	$this->view->posts = $this->db->getAnnouncements();
+	{	//$this->view->array_push("stylesheets", "http://localhost/zend/html/phpicalendar/templates/mathsoc/default.css");
 	}
 }
 
---
+++ application/default/views/scripts/events/index.tpl
@@ -0,0 +1,6 @@
+<iframe id='week'
+       name='week'
+            src='{$baseUrl}/phpicalendar/week.php'
+            width='800'
+            height='1000' align=left scrolling=no frameborder=0>
+</iframe>
